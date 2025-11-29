94683efd2406c39c773e1fa812f2bbb41b48c82a
Luca Ottaviano <lucatremila@gmail.com>
2025-11-29 14:30:04 +0100

    forward button added

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import "./shared-styles.scss";
 import {useRouter} from "next/navigation";
@@ -9,10 +9,21 @@ export default function ToolBar(props: ToolBarProps) {
   const router = useRouter();
   return (
     <div id="toolBar">
-      <button className="mainButton" onClick={()=> {
-        router.back()
-      }}>
+      <button
+        className="mainButton"
+        onClick={() => {
+          router.back();
+        }}
+      >
         <Image src="/icons/arrow-left.svg" alt="back" width={20} height={20} />
+      </button>
+      <button
+        className="mainButton"
+        onClick={() => {
+          router.forward();
+        }}
+      >
+        <Image src="/icons/arrow-right.svg" alt="back" width={20} height={20} />
       </button>
     </div>
   );
