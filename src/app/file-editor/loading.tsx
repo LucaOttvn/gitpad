be5938a347981ced8f2207e092ebd0d6de9206c0
Luca Ottaviano <lucatremila@gmail.com>
2025-12-26 12:30:14 +0100

    loader added

--- src/app/file-editor/loading.tsx
+++ src/app/file-editor/loading.tsx
@@ -1,71 +1,7 @@
-import "./style.scss";
-
 export default function FileEditorSkeleton() {
   return (
-    <div className="fileEditorSkeleton">
-      <div className="textLineSkeleton" style={{
-        width: '80%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '0%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '50%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '60%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '0%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '50%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '60%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '70%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '80%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '0%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '80%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '70%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '90%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '60%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '50%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '80%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '0%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '50%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '60%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '0%'
-      }}/>
-      <div className="textLineSkeleton" style={{
-        width: '50%'
-      }}/>
+    <div className="w-full h-full center">
+      <div className="loader"></div>
     </div>
   );
 }
--- src/app/file-explorer/loading.tsx
+++ src/app/file-explorer/loading.tsx
@@ -1,13 +1,8 @@
 import "./fileExplorer.scss";
 export default function FileExplorerSkeleton() {
   return (
-    <div className="fileExplorerSkeleton">
-      <div className="breadCrumbsSkeleton"></div>
-      <div className="fileExplorerListSkeleton">
-        {Array.from({length: 10}).map((_, i) => (
-          <div key={i} className="treeItemSkeleton"></div>
-        ))}
-      </div>
+    <div className="w-full h-full center">
+      <div className="loader"></div>
     </div>
   );
 }
