add8b5f1b4466057bb8a24a49b70c8a3ebdbe8d6
Luca Ottaviano <lucatremila@gmail.com>
2025-12-26 01:57:38 +0100

    update ui

--- src/components/toolbar/ExplorerToolBarButtons.tsx
+++ src/components/toolbar/ExplorerToolBarButtons.tsx
@@ -2,12 +2,9 @@ import Image from "next/image";
 import { useState } from "react";
 import BottomSheet from "../bottom-sheet/BottomSheet";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { PagesEnum } from "@/src/utils/enums";
 
 export default function ExplorerToolBarButtons() {
-  const pathName = usePathname();
-
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
 
   const handleBottomSheet = (value: boolean) => {
@@ -20,6 +17,12 @@ export default function ExplorerToolBarButtons() {
       <button onClick={() => setIsBottomSheetOpen(true)} className="mainButton clickableItem">
         <span>
           <Image src="/icons/add.svg" alt="folder" width={25} height={25} />
+        </span>
+      </button>
+      {/* Plus button */}
+      <button onClick={() => setIsBottomSheetOpen(true)} className="mainButton clickableItem">
+        <span>
+          <Image src="/icons/edit.svg" alt="folder" width={25} height={25} />
         </span>
       </button>
       {/* Settings button */}
