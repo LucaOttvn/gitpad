0d23bb866d2ae8d3c6482bcbaa9c296470c796c5
Luca Ottaviano <lucatremila@gmail.com>
2026-01-05 13:01:41 +0100

    docs

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,14 +3,13 @@ import { NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
 
 export async function proxy(req: NextRequest) {
-  // Read encrypted cookie
-  // Then return the custom JWT with the accessToken prop added by the NextAuth handler defined in app/api/auth/[...nextauth]/route.ts
+  // Read encrypted cookie, then return the custom JWT with the accessToken prop added by the NextAuth handler defined in app/api/auth/[...nextauth]/route.ts
   const token = await getToken({
     req,
     secret: process.env.AUTH_SECRET,
   })
 
-  // Redirect to the login page if the token isn't available
+  // Redirect to the login page if the token isn't available.
   if (!token) {
     const loginUrl = new URL("/login", req.url)
     return NextResponse.redirect(loginUrl)
@@ -28,8 +27,8 @@ export async function proxy(req: NextRequest) {
   return NextResponse.next()
 }
 
-// Only run on certain routes
+// Only run on certain routes (not the login page).
 export const config = {
-  matcher: ["/file-editor/:path*", "/file-explorer/:path*", "/settings/:path*"],
+  matcher: ["/file-editor/:path*", "/file-explorer/:path*", "/settings/:path*"]
 }
 
